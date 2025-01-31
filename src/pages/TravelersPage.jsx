934659d294e244aa05c7507f3e13679a4ddf1257
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-31 16:18:41 +0100

    feat: TravelersPage iniziata
    Co-authored-by: angeloalmanza <angeloalmanza@users.noreply.github.com>

---
+++ src/pages/TravelersPage.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function TravelersPage() {
+// Creo lo state per settare la lista in base al viaggio cliccato nella pagina home
+const [travelers, setTravelers] = useState([]);
+
+// creo la chiamata in entrata per prendere i dati
+const getTravelers = () => {
+    
+}
+
+
+
+const [search, Setsearch] = useState("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return(
+    <>
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    </>
+)
+
+
+}
+
+export {TravelersPage};
