67ece807cacdba13d70f1547926b8c1470a23ca4
adrianopiacentini <adrianopiacentini03@gmail.com>
2025-01-31 16:13:30 +0100

    feat: layout scaffolding

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,9 +1,13 @@
+import { Outlet } from "react-router-dom";
+import AppHeader from "./AppHeader";
+import AppFooter from "./AppFooter";
+
 const AppLayout = () => {
     return (
         <>
-        <h1>App Layout</h1>
-        <h1>Ciao a tutti da Alessandro</h1>
-        <h2>Piero</h2>
+            <AppHeader />
+            <Outlet />
+            <AppFooter />
         </>
     )
 }
