682f5b46deca9f60ee5ff537bf1514732841b1f0
mbaisotti99 <matteo.baisotti@gmail.com>
2025-01-31 17:31:37 +0100

    Bugfix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,10 +3,8 @@ import { NavLink } from "react-router-dom"
 function NavBar() {
     return (
         <>
-            <nav className="navbar bg-body-tertiary navstyle">
-                    <NavLink className="navbar-brand" to="/">
-                        <img src="https://venezia-samarcanda.it/wp-content/uploads/2017/06/weroad.png" alt="Bootstrap" width="300" height="200" />
-                    </NavLink>
+            <nav className="navbar bg-body-tertiary navstyle navbar-brand">
+                        <img src="https://venezia-samarcanda.it/wp-content/uploads/2017/06/weroad.png" alt="Weroad" className="homeImg" />
             </nav>
         </>
     )
