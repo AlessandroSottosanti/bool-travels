1d716303d12b1935c45cb7b6f9e4aaeb1a94bfc6
adrianopiacentini <adrianopiacentini03@gmail.com>
2025-01-31 17:16:32 +0100

    feat: AppFooter  & index.css changes

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,9 @@
 function AppFooter() {
     return (
         <>
+            <footer className='bg-dark text-white text-center py-3 mt-auto'>
+                <p className='m-0'>Bool Travels</p>
+            </footer>
         </>
     )
 }
