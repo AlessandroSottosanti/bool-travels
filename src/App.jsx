0a3c6e10ded7026e0299e49f2aa5adc256379073
AlessandroSottosanti <asottosanti2@gmail.com>
2025-01-31 13:30:06 +0100

    feat: app config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes >
+          <Route element={<AppLayout />}>
+            <Route index element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
