93f5723eded45eac079ecd34454b95b6ed74fb3d
mbaisotti99 <matteo.baisotti@gmail.com>
2025-01-31 17:05:01 +0100

    App Header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,13 @@
+import { NavLink } from "react-router-dom"
+
 function NavBar() {
     return (
         <>
+            <nav class="navbar bg-body-tertiary navstyle">
+                    <NavLink class="navbar-brand" to="/">
+                        <img src="https://venezia-samarcanda.it/wp-content/uploads/2017/06/weroad.png" alt="Bootstrap" width="300" height="200" />
+                    </NavLink>
+            </nav>
         </>
     )
 }
