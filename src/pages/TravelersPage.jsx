2ad4f86e4fbdc40f0d098e3d54ba51aa1a62d711
BartCatalano <catalanobartolomeo@gmail.com>
2025-01-31 16:30:03 +0100

    feat: map start

--- src/pages/TravelersPage.jsx
+++ src/pages/TravelersPage.jsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
+import viaggi from "../data/viaggi"
+const TravelersPage = () => {
+// // Creo lo state per settare la lista in base al viaggio cliccato nella pagina home
+// const [travelers, setTravelers] = useState([]);
 
-function TravelersPage() {
-// Creo lo state per settare la lista in base al viaggio cliccato nella pagina home
-const [travelers, setTravelers] = useState([]);
-
-// creo la chiamata in entrata per prendere i dati
-const getTravelers = () => {
+// // creo la chiamata in entrata per prendere i dati, sarà attiva quando si avrà un back
+// const getTravelers = () => {
     
-}
+// }
 
-
+const viaggio = viaggi[0]; 
 
 const [search, Setsearch] = useState("");
+const viaggiatoriFiltrati = viaggi
+.flatMap((viaggio) => viaggio.viaggiatori)
+.filter((viaggiatore) => viaggiatore.cognome.toLowerCase().includes(search.toLowerCase()));
 
 
 
@@ -30,7 +33,18 @@ const [search, Setsearch] = useState("");
 
 return(
     <>
-    
+    <div>
+          <h2>nome Viaggio</h2>
+          {viaggio.ma
+            
+          }
+  
+
+
+
+
+
+    </div>
 
 
 
@@ -57,4 +71,4 @@ return(
 
 }
 
-export {TravelersPage};
+export default TravelersPage;
